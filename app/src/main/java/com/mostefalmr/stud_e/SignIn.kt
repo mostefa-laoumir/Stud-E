6f9b7e82a9fc25f58ad588d78d4845beeac5faee
Mostefa <21nounou21@gmail.com>
2022-03-31 20:30:53 +0100

    initial commit

---
+++ app/src/main/java/com/mostefalmr/stud_e/SignIn.kt
@@ -0,0 +1,14 @@
+package com.mostefalmr.stud_e
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        supportActionBar!!.hide()
+
+        
+    }
+}
